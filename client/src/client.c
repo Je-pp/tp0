eed32ad73536e4d49bb7f470f0d7512e8648487d
Julio Patino <jpatinoparra@frba.utn.edu.ar>
2025-04-16 18:40:37 +0000

    ultimo commit, comunicacion entre serv-cli

--- client/src/client.c
+++ client/src/client.c
@@ -53,11 +53,14 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
-	enviar_mensaje("VALORAME",conexion);
-	// liberar_conexion(conexion);
+	enviar_mensaje(valor,conexion);
+
+	// liberar_conexion(conexion); // no se si esto aun.
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
+
+	//terminamos el programa
 
 	terminar_programa(conexion, logger, config);
 
@@ -104,13 +107,23 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	leido = readline("> ");
+	while(strcmp(leido, "") != 0){
+		agregar_a_paquete(paquete, leido, strlen(leido)+1);
+		free(leido);
+		leido = readline("> ");
+	};
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+
+	//enviamos el paquete
+	enviar_paquete(paquete,conexion);
+	//eliminamos el paquete
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -119,4 +132,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	  log_destroy(logger);
 	  config_destroy(config);
+	  liberar_conexion(conexion);
 }
